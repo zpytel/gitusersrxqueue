c016cb691033803cecbb788908d67ce26310f01f
zpytel <zbigniew.pytel@sickkids.ca>
2017-01-11 15:21:35 -0500

    Create Items insteada of Cars

--- routes/queue.js
+++ routes/queue.js
@@ -8,9 +8,59 @@ admin.initializeApp({
 });
 /* GET home page. */
 var ref=admin.database().ref('queue');
-var refcar=admin.database().ref('/cars');
-
+//var refcar=admin.database().ref('/cars');
+function keyillnessExists(illness,exists,snap,db,data){
+    
+     if(exists){
+        let illness=data.part + '|' + data.illness;
+        let key;
+        for(k in snap.val()){
+            key=k;
+        }
+        var updated=snap.val()[key].count+1;
+         let fullPath=admin.database().ref('/items/keyillness/'  + data.keyword + '/' + illness + '/' + key )
+        fullPath.update({count:updated})
+        console.log('keyillnes: ' + illness + ' exists')
+    }else{
+        var refnew=db.push();
+        refnew.set({count:1});
+        console.log('keyillnes: ' + illness + ' not exists')
+    }
+}
+function illnesskeyExists(illness,exists,snap,db,data){
+    if(exists){
+        let illness=data.part + '|' + data.illness;
+        let key;
+        for(k in snap.val()){
+            key=k;
+        }
+        var updated=snap.val()[key].count+1;
+        let fullPath=admin.database().ref('/items/illnesskey/' + illness + '/' + data.keyword + '/' + key )
+        fullPath.update({count:updated})
+        
+        console.log('illnesskey: ' + illness + ' exists')
+    }else{
+        var refnew=db.push();
+        refnew.set({count:1});
+        console.log('illnesskey: ' + illness + ' not exists')
+    }
+}
 var queue=new Queue(ref,function(data,progress,resolve,reject){
+    let illness=data.part + '|' + data.illness;
+    let illnesskey=admin.database().ref('/items/illnesskey/' + illness + '/' + data.keyword );
+    let keyillness=admin.database().ref('/items/keyillness/' + data.keyword + '/' + illness);
+    
+    illnesskey.once('value',function(snapshot){
+      var exists=(snapshot.val()!=null);
+       console.log(snapshot.val())
+       illnesskeyExists(illness,exists,snapshot,illnesskey,data)
+    })
+    keyillness.once('value',function(snapshot){
+        var exists=(snapshot.val()!=null);
+        console.log(snapshot.val())
+       keyillnessExists(illness,exists,snapshot,keyillness,data)
+    })
+/*
     let maxid=0;
     let newcar=refcar.push();
     refcar.once('value',function(snapshot){
@@ -25,8 +75,9 @@ var queue=new Queue(ref,function(data,progress,resolve,reject){
       })
       maxid=maxid+1
        newcar.set({id:maxid,make:data.make,model:data.model})
+      
        progress(50);
-    });
+    }); */
     
    
    
