094cd290d5a5784be26fd0bab9e057e0c23cb504
zpytel <zbigniew.pytel@sickkids.ca>
2017-01-13 13:43:22 -0500

    User check and message writed to db

--- routes/queue.js
+++ routes/queue.js
@@ -9,6 +9,7 @@ admin.initializeApp({
 /* GET home page. */
 var ref=admin.database().ref('queue');
 //var refcar=admin.database().ref('/cars');
+
 function keyillnessExists(illness,exists,snap,db,data){
     
      if(exists){
@@ -45,21 +46,56 @@ function illnesskeyExists(illness,exists,snap,db,data){
         console.log('illnesskey: ' + illness + ' not exists')
     }
 }
-var queue=new Queue(ref,function(data,progress,resolve,reject){
-    let illness=data.part + '|' + data.illness;
-    let illnesskey=admin.database().ref('/items/illnesskey/' + illness + '/' + data.keyword );
-    let keyillness=admin.database().ref('/items/keyillness/' + data.keyword + '/' + illness);
-    
-    illnesskey.once('value',function(snapshot){
-      var exists=(snapshot.val()!=null);
-       console.log(snapshot.val())
-       illnesskeyExists(illness,exists,snapshot,illnesskey,data)
-    })
-    keyillness.once('value',function(snapshot){
+var options={
+    'specId':'verify_user'
+}
+var verifyqueue=new Queue(ref,options,function(data,progress,resolve,reject){
+let illness=data.part + '|' + data.illness;
+let illnesskeymsg=admin.database().ref('/items/illnesskeymsg/' + illness + '/' + data.keyword +'/' + data.user );
+    illnesskeymsg.once('value',function(snapshot){
+         var exists=(snapshot.val()!=null);
+         console.log("Exists:" + exists)
+         if (exists==true){
+             console.log("user exists")
+             resolve({'notvalid':true})
+             
+         }else{
+           var refnew=illnesskeymsg.push();
+           refnew.set({howto:data.message})
+           resolve(data);
+         }
+         
+    });
+})
+var optionsMain={
+    'specId':'add_items'
+}
+
+process.on('SIGINT', function() {
+  console.log('Starting queue shutdown');
+  queue.shutdown().then(function() {
+    console.log('Finished queue shutdown');
+    process.exit(0);
+  });
+});
+var queue=new Queue(ref,optionsMain,function(data,progress,resolve,reject){
+    if(!data.notvalid){
+        let illness=data.part + '|' + data.illness;
+        let illnesskey=admin.database().ref('/items/illnesskey/' + illness + '/' + data.keyword );
+        let keyillness=admin.database().ref('/items/keyillness/' + data.keyword + '/' + illness);
+        
+        illnesskey.once('value',function(snapshot){
         var exists=(snapshot.val()!=null);
         console.log(snapshot.val())
-       keyillnessExists(illness,exists,snapshot,keyillness,data)
-    })
+        illnesskeyExists(illness,exists,snapshot,illnesskey,data)
+        })
+        keyillness.once('value',function(snapshot){
+            var exists=(snapshot.val()!=null);
+            console.log(snapshot.val())
+        keyillnessExists(illness,exists,snapshot,keyillness,data)
+        })
+
+    }//process data
 /*
     let maxid=0;
     let newcar=refcar.push();
